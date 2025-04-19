96a1d451563f1d5856dcd50b3ae1e226e5f562aa
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-04-18 22:26:56 -0500

    adding Categoria model

---
+++ src/main/java/co/ufps/pdau/controller/CategoriaController.java
@@ -0,0 +1,50 @@
+package co.ufps.pdau.controller;
+
+import co.ufps.pdau.model.Categoria;
+import co.ufps.pdau.service.CategoriaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+@RequiredArgsConstructor
+public class CategoriaController {
+    private final CategoriaService categoriaService;
+
+    @GetMapping("/list")
+    public List<Categoria> getAllCategorias() {
+        return categoriaService.getAllCategorias();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> getCategoriaById(@PathVariable Long id) {
+        return categoriaService.getCategoriaById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Categoria createCategoria(@RequestBody Categoria categoria) {
+        return categoriaService.createCategoria(categoria);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Categoria> updateCategoria(@PathVariable Long id, @RequestBody Categoria categoriaDetails) {
+        try {
+            Categoria updatedCategoria = categoriaService.updateCategoria(id, categoriaDetails);
+            return ResponseEntity.ok(updatedCategoria);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategoria(@PathVariable Long id) {
+        categoriaService.deleteCategoria(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
