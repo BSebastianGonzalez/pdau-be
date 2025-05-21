032a9c251ac1d3aef1c88db7922164b41fe82895
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-05-21 18:51:39 -0500

    hotfix: fixing bad url path

--- src/main/java/co/ufps/pdau/config/WebConfig.java
+++ src/main/java/co/ufps/pdau/config/WebConfig.java
@@ -14,10 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173",
-                        "https://pdau-fe.vercel.app"
-                )
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
