22f75a7d815c1ce79c201c86a306224a30d4aa63
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-09-11 16:06:00 -0500

    fix: changing database

---
+++ src/main/java/co/ufps/pdau/DTO/CategoriaDTO.java
@@ -0,0 +1,13 @@
+package co.ufps.pdau.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoriaDTO {
+    private String titulo;
+    private String descripcion;
+}
